75bd347d6358b42744f49e832cdd3cccee902bda
Efaz Rahman <123217968+efazman@users.noreply.github.com>
2025-08-06 12:18:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -50,8 +50,6 @@ function displayCat() {
     catImage.src = 'EJ.png'; // Assuming the cat image is named "EJ.png"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
-    catImage.width = 10;  // Set your desired width in pixels
-    catImage.height = 10;
     // When the cat image is fully loaded, add it to the image container
     catImage.onload = function() {
         imageContainer.appendChild(catImage);
